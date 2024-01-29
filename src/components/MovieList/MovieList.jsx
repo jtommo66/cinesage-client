5fde1aa94f573d86aa11caa0c490338328d6f53f
Jack Tomlinson <jtommo66@hotmail.com>
2024-01-29 21:30:24 +0000

    add link to movie images

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,19 +1,20 @@
 import "./MovieList.scss";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
+const URL = process.env.url;
 
 function MovieList() {
-  const { movieId } = useParams;
+  const { movieId } = useParams();
   const [movieList, setMovieList] = useState(null);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const movies = await axios.get(`${API_URL}/movies`);
-        setMovieList(movies);
+        setMovieList(movies.data);
       } catch (error) {
         console.error(error);
       }
@@ -21,11 +22,26 @@ function MovieList() {
     fetchMovies();
   }, [movieId]);
 
-  if (!MovieList) {
+  if (!movieList) {
     return <main>Finding Movies...</main>;
   }
 
-  return <main></main>;
+  return (
+    <main className="movie-list">
+      {movieList.map((movie) => (
+        <article key={movie.id} className="movie-list__movie">
+          <Link to={`/movies/${movie.id}`}>
+            <img
+              className="movie-list__movie-image"
+              src={movie.image}
+              alt="movie poster"
+            />
+          </Link>
+          <h2 className="movie-list__movie-title">{movie.title}</h2>
+        </article>
+      ))}
+    </main>
+  );
 }
 
 export default MovieList;
