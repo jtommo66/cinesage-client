e7b5802494e9115c28cfa712f6f4d7943d4e7282
Jack Tomlinson <jtommo66@hotmail.com>
2024-02-05 11:34:30 +0000

    add login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import HomePage from "./pages/Home/Home";
 import SingleMoviePage from "./pages/SingleMovie/SingleMovie";
 import MovieListPage from "./pages/MovieList/MovieList";
 import Header from "./components/Header/Header";
+import LoginPage from "./pages/Login/Login";
+import RegisterPage from "./pages/Register/Register";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/roulette" element={<RoulettePage />} />
         <Route path="/movies/:movieId" element={<SingleMoviePage />} />
         <Route path="/movies" element={<MovieListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,45 @@
+import Input from "../../components/Input/Input";
+import "./Register.scss";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+function Register() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post(`${API_URL}/register`, {
+        name: event.target.name.value,
+        email: event.target.email.value,
+        password: event.target.password.value,
+      });
+      navigate("/login");
+    } catch (error) {
+      event.target.reset();
+      setError(error.response.data);
+    }
+  };
+
+  return (
+    <main className="signup-page">
+      <form className="signup" onSubmit={handleSubmit}>
+        <h1 className="signup__title">Sign up</h1>
+        <Input type="text" name="name" label="Name" />
+        <Input type="text" name="email" label="Email" />
+        <Input type="password" name="password" label="Password" />
+        <button className="signup__button">Sign up</button>
+        <p>{error}</p>
+      </form>
+      <p>
+        Have an account? <Link to="/login">Log in</Link>
+      </p>
+    </main>
+  );
+}
+
+export default Register;
