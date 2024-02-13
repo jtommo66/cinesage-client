f2dbdc322af55939b0183167c061618a94af2b44
Jack Tomlinson <jtommo66@hotmail.com>
2024-02-13 17:39:42 +0000

    add conditional rendering to header login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,30 +3,59 @@ import logo from "../../assets/images/CineSage-logos.jpeg";
 import { NavLink, Link } from "react-router-dom";
 
 function Header() {
-  return (
-    <header className="header">
-      <div className="header__wrap">
-        <div className="header__logo-wrap">
-          <Link to="/">
-            <img className="header__logo" src={logo} alt="CineSage Logo" />
-          </Link>
-        </div>
-        <div className="header__nav-background">
-          <div className="header__nav-wrap">
-            <NavLink className="header__link" to="/roulette">
-              Movie Roulette
-            </NavLink>
-            <NavLink className="header__link" to="/movies">
-              Movies
-            </NavLink>
-            <NavLink className="header__link" to="/login">
-              Log In
-            </NavLink>
+  const token = sessionStorage.getItem("token");
+
+  if (!token) {
+    return (
+      <header className="header">
+        <div className="header__wrap">
+          <div className="header__logo-wrap">
+            <Link to="/">
+              <img className="header__logo" src={logo} alt="CineSage Logo" />
+            </Link>
+          </div>
+          <div className="header__nav-background">
+            <div className="header__nav-wrap">
+              <NavLink className="header__link" to="/roulette">
+                Movie Roulette
+              </NavLink>
+              <NavLink className="header__link" to="/movies">
+                Movies
+              </NavLink>
+              <NavLink className="header__link" to="/login">
+                Log In
+              </NavLink>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-  );
+      </header>
+    );
+  } else {
+    return (
+      <header className="header">
+        <div className="header__wrap">
+          <div className="header__logo-wrap">
+            <Link to="/">
+              <img className="header__logo" src={logo} alt="CineSage Logo" />
+            </Link>
+          </div>
+          <div className="header__nav-background">
+            <div className="header__nav-wrap">
+              <NavLink className="header__link" to="/roulette">
+                Movie Roulette
+              </NavLink>
+              <NavLink className="header__link" to="/movies">
+                Movies
+              </NavLink>
+              <NavLink className="header__link" to="/">
+                Welcome Back!
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
